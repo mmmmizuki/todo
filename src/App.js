0bfbc87b9671fc4b7a9acd2d4b6ce384b153edbf
mmmmizuki <fettuccine03@gmail.com>
2022-09-07 17:25:55 +0900

    todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [tasks,setTasks]=useState([])
+  const [labelInput,setLabelInput]=useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="input-container">
+        <input className='label-input' onChange={(event)=>{
+          setLabelInput(event.currentTarget.value)
+        }}value={labelInput}/>
+        <button className="add-button" onClick={()=>{
+          setTasks([{checked:false,label:labelInput},...tasks])
+          setLabelInput("")
+        }}>追加</button>
+      </div>
+
+      <div className="task-list">
+        {
+          tasks.map((task)=>{
+            return (
+              <div className={task.checked?'task checked':'task'}>
+                <input type="checkbox" checked={task.checked} onChange={(event)=>{
+                  task.checked=event.currentTarget.checked
+                  console.log(event.currentTarget.checked)
+                  console.log(task)
+                  tasks.sort((a,b)=>{
+                    if(a.checked === b.checked){
+                      return 0
+                    }
+                    if(a.checked){
+                      return 1
+                    }
+                    return -1
+                  })
+                  setTasks([...tasks])
+                }}/>
+                <div className="task-label">{task.label}</div>
+                <button onClick={()=>{
+                  const index =tasks.indexOf(task)
+                  tasks.splice(index,1)
+                  setTasks([...tasks])
+                }}>×</button>
+              </div>
+            )
+          })
+        }
+        
+        
+
+      </div>
     </div>
   );
 }
